2b3b7183543c36b880e605fe9215501ce9cd5761
mnyxcrack <mnyxcrack@gmail.com>
2026-04-28 11:37:31 +0200

    Update logger.js

--- f4nabot/src/utils/logger.js
+++ f4nabot/src/utils/logger.js
@@ -1,9 +1,8 @@
 const chalk = require('chalk');
-const gradient = require('gradient-string');
 
 module.exports = {
-    start: (msg) => console.log(gradient.pastel(`[START] ${msg}`)),
-    success: (msg) => console.log(chalk.green(`✔ ${msg}`)),
-    error: (msg) => console.log(chalk.red(`✖ ${msg}`)),
-    info: (msg) => console.log(chalk.cyan(`➜ ${msg}`))
+    success: (msg) => console.log(chalk.green(msg)),
+    error: (msg) => console.log(chalk.red(msg)),
+    warn: (msg) => console.log(chalk.yellow(msg)), // ✅ AJOUT ICI
+    info: (msg) => console.log(chalk.blue(msg))
 };
