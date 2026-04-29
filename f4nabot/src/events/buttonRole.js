e28a79c487001b964092d97fb27babe29c0d58eb
mnyxcrack <mnyxcrack@gmail.com>
2026-04-29 03:28:26 +0200

    Update buttonRole.js

--- f4nabot/src/events/buttonRole.js
+++ f4nabot/src/events/buttonRole.js
@@ -1,1 +1,36 @@
 
+module.exports = {
+    name: 'interactionCreate',
+
+    async execute(interaction) {
+
+        if (!interaction.isButton()) return;
+
+        if (interaction.customId === 'accept_reglement') {
+
+            const roleId = "1496955671852351528";
+            const role = interaction.guild.roles.cache.get(roleId);
+
+            if (!role) {
+                return interaction.reply({
+                    content: "❌ Rôle introuvable",
+                    ephemeral: true
+                });
+            }
+
+            if (interaction.member.roles.cache.has(roleId)) {
+                return interaction.reply({
+                    content: "✅ Déjà validé",
+                    ephemeral: true
+                });
+            }
+
+            await interaction.member.roles.add(role).catch(() => {});
+
+            await interaction.reply({
+                content: "✅ Règlement accepté !",
+                ephemeral: true
+            });
+        }
+    }
+};
