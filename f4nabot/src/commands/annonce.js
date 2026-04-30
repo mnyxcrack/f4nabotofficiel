94f1aeb40d30a6dfdaea98a66deea0736bddad42
mnyxcrack <mnyxcrack@gmail.com>
2026-04-30 21:41:43 +0200

    Add files via upload

---
+++ f4nabot/src/commands/annonce.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('annonce')
+        .setDescription('Créer une annonce'),
+
+    async execute(interaction) {
+
+        const modal = new ModalBuilder()
+            .setCustomId('annonceModal')
+            .setTitle('Créer une annonce');
+
+        const titre = new TextInputBuilder()
+            .setCustomId('titre')
+            .setLabel('Titre')
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+
+        const sousTitre = new TextInputBuilder()
+            .setCustomId('sousTitre')
+            .setLabel('Sous-titre')
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
+
+        const description = new TextInputBuilder()
+            .setCustomId('description')
+            .setLabel('Description')
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(true);
+
+        modal.addComponents(
+            new ActionRowBuilder().addComponents(titre),
+            new ActionRowBuilder().addComponents(sousTitre),
+            new ActionRowBuilder().addComponents(description)
+        );
+
+        await interaction.showModal(modal);
+    }
+};
